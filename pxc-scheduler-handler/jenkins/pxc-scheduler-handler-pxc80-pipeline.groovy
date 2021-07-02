5c0dc65a5ac8fe2144a812f5109c26b031131314
Marco Tusa <tusamarco@gmail.com>
2021-07-02 16:24:15 +0200

    space after slash

--- pxc-scheduler-handler/jenkins/pxc-scheduler-handler-pxc80-pipeline.groovy
+++ pxc-scheduler-handler/jenkins/pxc-scheduler-handler-pxc80-pipeline.groovy
@@ -47,7 +47,7 @@ pipeline {
                     sudo git reset --hard
                     sudo git clean -xdf
                     sudo rm -rf sources
-                    ./ pxc-scheduler-handler/local/checkout
+                    ./pxc-scheduler-handler/local/checkout
                 '''
 
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
